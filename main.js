01fc929e788d042267159b5aa538013e7ff175d4
itskharina <137315976+itskharina@users.noreply.github.com>
2024-01-12 05:10:15 +0000

    Cleaned up code and made form validation better

--- main.js
+++ main.js
@@ -1,38 +1,64 @@
-let password = document.querySelector('#password')
-let confirm = document.querySelector('#confirm-password')
-let passwordError = document.querySelector('.password-error')
-let email = document.querySelector('#email')
-let emailError = document.querySelector('.email-error')
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+const passwordError = document.querySelector('.password-error');
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+const submit = document.querySelector('.submit');
+const phone = document.querySelector('#phone');
+const phoneError = document.querySelector('.phone-error');
 
-// checks if password and confirm password are the same
-function check() {
-    if (password.value === confirm.value) {
-        confirm.style.outline = "none"
-        passwordError.textContent = ""
-    } else {
-        confirm.style.outline = "1px solid red"
-        passwordError.textContent = "*Passwords must match"
-    }
+submit.addEventListener('click', checkValidity);
+password.addEventListener('change', checkPassword);
+confirmPassword.addEventListener('change', checkPassword);
+email.addEventListener('input', checkEmail);
+phone.addEventListener('input', checkPhone);
+
+function checkValidity(e) {
+  if (password.value !== confirmPassword.value) {
+    alert('Please make sure your passwords match!');
+    e.preventDefault();
+    return;
+  }
+
+  if (!/^[0-9]{11}$/.test(phone.value)) {
+    alert('Please enter a valid UK phone number!');
+    e.preventDefault();
+    return;
+  }
+
+  if (!password.value || !confirmPassword.value) {
+    alert('Please enter a password!');
+    e.preventDefault();
+    return;
+  }
 }
 
-// stops the outlines from css from disappearing/overlapping with the ones created in js when input is focused  
-confirm.addEventListener("focus", (e) => {
-    e.target.style.outline = "1px solid #E5E7EB"
-});
+function checkPassword() {
+  if (password.value === confirmPassword.value) {
+    confirmPassword.style.border = '1px solid #E5E7EB';
+    passwordError.textContent = '';
+  } else {
+    confirmPassword.style.border = '1px solid red';
+    passwordError.textContent = '*Passwords must match!';
+  }
+}
 
-confirm.addEventListener("blur", (e) => {
-    if (password.value === confirm.value) {
-        e.target.style.outline = "none"
-    } else {
-        e.target.style.outline = "1px solid red"
-    }
-});
+function checkEmail() {
+  if (email.validity.typeMismatch) {
+    emailError.textContent = '*Enter a valid email';
+    email.style.border = '1px solid red';
+  } else {
+    emailError.textContent = '';
+    email.style.border = '1px solid #E5E7EB';
+  }
+}
 
-email.addEventListener("input", function (e) {
-    if (email.validity.typeMismatch) {
-        emailError.textContent = "*Please enter a valid email";
-    } else {
-        emailError.textContent = '';
-    }
-})
-
+function checkPhone() {
+  if (/^[0-9]{11}$/.test(phone.value)) {
+    phoneError.textContent = '';
+    phone.style.border = '1px solid #E5E7EB';
+  } else {
+    phoneError.textContent = '*Enter a valid UK phone number';
+    phone.style.border = '1px solid red';
+  }
+}
